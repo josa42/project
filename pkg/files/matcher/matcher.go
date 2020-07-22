5c2b06db8c9c88be7978be9d60b79e0a577423ff
Josa Gesell <josa@gesell.me>
2020-07-22 23:30:47 +0200

    Add FilePattern type

--- pkg/files/matcher/matcher.go
+++ pkg/files/matcher/matcher.go
@@ -1,7 +1,9 @@
 package matcher
 
 import (
+	"fmt"
 	"os"
+	"path/filepath"
 	"regexp"
 	"sort"
 	"strings"
@@ -13,8 +15,7 @@ var (
 	wildcards = regexp.MustCompile(`{(\*\*?)(\|([^:}]+))?(:([a-z]+))?}`)
 )
 
-func FindFiles(dir, pattern string) []string {
-
+func findFiles(dir, pattern string) []string {
 	globPattern := toGlobPattern(pattern)
 
 	defer cd(dir)()
@@ -26,22 +27,69 @@ func FindFiles(dir, pattern string) []string {
 }
 
 func toGlobPattern(pattern string) string {
-
 	matches := wildcards.FindAllStringSubmatch(pattern, -1)
 
 	for _, m := range matches {
-		pattern = strings.Replace(pattern, m[0], starReplace(m[1]), 1)
+		pattern = strings.Replace(pattern, m[0], starToGlob(m[1]), 1)
 	}
 
 	return pattern
 }
 
-func starReplace(star string) string {
+func toExpr(pattern string) string {
+	matches := wildcards.FindAllStringSubmatch(pattern, -1)
+
+	replaces := map[string]string{}
+
+	for idx, m := range matches {
+		placeholder := fmt.Sprintf(`::::####%d####:::`, idx)
+		pattern = strings.Replace(pattern, m[0], placeholder, 1)
+
+		replaces[placeholder] = starToExpr(m[1])
+	}
+
+	pattern = regexp.QuoteMeta(pattern)
+
+	for placeholder, replace := range replaces {
+		pattern = strings.Replace(pattern, placeholder, replace, 1)
+	}
+
+	return pattern
+}
+
+func groupNames(pattern string) map[int]string {
+	matches := wildcards.FindAllStringSubmatch(pattern, -1)
+
+	groups := map[int]string{}
+
+	for idx, m := range matches {
+		groups[idx] = m[5]
+	}
+
+	if len(groups) == 1 && groups[0] == "" {
+		groups[0] = "path"
+	}
+
+	return groups
+}
+
+func starToGlob(star string) string {
 	switch star {
 	case "*":
 		return "*"
 	case "**":
 		return "**/*"
+	default:
+		return star
+	}
+}
+
+func starToExpr(star string) string {
+	switch star {
+	case "*":
+		return fmt.Sprintf(`([^%c]+)`, filepath.Separator)
+	case "**":
+		return `(.+)`
 	default:
 		return star
 	}
@@ -54,4 +102,47 @@ func cd(dir string) func() {
 	return func() {
 		os.Chdir(pwd)
 	}
+}
+
+type FilePattern string
+
+func (fp FilePattern) Find(dir string) []string {
+	pattern := toGlobPattern(string(fp))
+	return findFiles(dir, pattern)
+}
+
+func (fp FilePattern) Match(path string) map[string]string {
+
+	gn := groupNames(string(fp))
+	exp := regexp.MustCompile(toExpr(string(fp)))
+
+	matches := exp.FindAllStringSubmatch(path, -1)
+
+	if len(matches) > 0 {
+		groups := map[string]string{}
+
+		for idx, m := range matches {
+			name := gn[idx]
+			groups[name] = m[1]
+		}
+
+		return groups
+
+	}
+
+	return nil
+}
+
+func (fp FilePattern) Fill(groups map[string]string) (string, error) {
+
+	matches := wildcards.FindAllStringSubmatch(string(fp), -1)
+	gn := groupNames(string(fp))
+
+	path := string(fp)
+	for idx, m := range matches {
+		name := gn[idx]
+		path = strings.Replace(path, m[0], groups[name], 1)
+	}
+
+	return path, nil
 }
--- pkg/files/matcher/matcher_test.go
+++ pkg/files/matcher/matcher_test.go
@@ -22,7 +22,7 @@ func TestFindFiles(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := FindFiles(tt.args.dir, tt.args.pattern); !reflect.DeepEqual(got, tt.want) {
+			if got := findFiles(tt.args.dir, tt.args.pattern); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("FindFiles() = %v, want %v", got, tt.want)
 			}
 		})
@@ -49,6 +49,146 @@ func Test_toGlobPattern(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := toGlobPattern(tt.args.pattern); got != tt.want {
 				t.Errorf("toGlobPattern() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_toExpr(t *testing.T) {
+	type args struct {
+		pattern string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"empty", args{""}, ""},
+		{"single star", args{`dir/{*}.js`}, `dir/([^/]+)\.js`},
+		{"double star", args{`dir/{**}.js`}, `dir/(.+)\.js`},
+		{"transform", args{`dir/{*|dashed}.js`}, `dir/([^/]+)\.js`},
+		{"named", args{`dir/{*:path}.js`}, `dir/([^/]+)\.js`},
+		{"named 2", args{`{**:path}/file.js`}, `(.+)/file\.js`},
+		{"transform and named", args{`dir/{*|dashed:path}.js`}, `dir/([^/]+)\.js`},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := toExpr(tt.args.pattern); got != tt.want {
+				t.Errorf("toExpr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_groupNames(t *testing.T) {
+	type args struct {
+		pattern string
+	}
+	tests := []struct {
+		name string
+		args args
+		want map[int]string
+	}{
+		{"empty", args{""}, map[int]string{}},
+		{"named", args{"{**:path}/file.js"}, map[int]string{0: "path"}},
+		{"transformed", args{"{**|dashed}/file.js"}, map[int]string{0: "path"}},
+		{"named and transformed", args{"{**|dashed:path}/file.js"}, map[int]string{0: "path"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := groupNames(tt.args.pattern); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("groupNames() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFilePattern_Match(t *testing.T) {
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name string
+		fp   FilePattern
+		args args
+		want map[string]string
+	}{
+		{"empty", "{**}", args{""}, nil},
+		{"named", "{**:path}/file.js", args{"foo/bar/file.js"}, map[string]string{"path": "foo/bar"}},
+		{"transformed", "{**|dashed}/file.js", args{"foo/bar/file.js"}, map[string]string{"path": "foo/bar"}},
+		{"named and transformed", "{**|dashed:path}/file.js", args{"foo/bar/file.js"}, map[string]string{"path": "foo/bar"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.fp.Match(tt.args.path); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FilePattern.Match() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_starToGlob(t *testing.T) {
+	type args struct {
+		star string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := starToGlob(tt.args.star); got != tt.want {
+				t.Errorf("starToGlob() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_starToExpr(t *testing.T) {
+	type args struct {
+		star string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := starToExpr(tt.args.star); got != tt.want {
+				t.Errorf("starToExpr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFilePattern_Fill(t *testing.T) {
+	type args struct {
+		groups map[string]string
+	}
+	tests := []struct {
+		name    string
+		fp      FilePattern
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{"basic", "src/{*}.js", args{map[string]string{"path": "foo"}}, "src/foo.js", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.fp.Fill(tt.args.groups)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FilePattern.Fill() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("FilePattern.Fill() = %v, want %v", got, tt.want)
 			}
 		})
 	}
