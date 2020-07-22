84fa18c8ab6295101ad917580e60a3e84ed52812
Josa Gesell <josa@gesell.me>
2020-07-23 00:19:09 +0200

    ✨ Add basic find command

---
+++ cmd/find.go
@@ -0,0 +1,40 @@
+/*
+Copyright © 2020 Josa Gesell <josa@gesell.me>
+
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/josa42/project/pkg/out"
+	"github.com/josa42/project/pkg/project"
+	"github.com/spf13/cobra"
+)
+
+// findCmd represents the find command
+var findCmd = &cobra.Command{
+	Use:   "find",
+	Short: "",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		projPath := project.FindProjeFile(".")
+		if projPath == "" {
+			out.Log("Could not find project.yml")
+			os.Exit(1)
+		}
+
+		proj := project.LoadProjeFile(projPath)
+		key := args[0]
+
+		for _, f := range proj.FindFiles(key) {
+			fmt.Println(f)
+		}
+	},
+	Args: cobra.ExactArgs(1),
+}
+
+func init() {
+	rootCmd.AddCommand(findCmd)
+}
--- pkg/project/project.go
+++ pkg/project/project.go
@@ -51,6 +51,22 @@ type Project struct {
 	Tasks map[string]Task     `yaml:"tasks"`
 }
 
+func (p Project) FindFiles(key string) []string {
+	files := []string{}
+
+	ft := p.Files[key]
+
+	for _, fp := range ft.PathPatterns {
+		for _, f := range fp.Find(".") {
+			if !ft.isExcluded(f) {
+				files = append(files, f)
+			}
+		}
+	}
+
+	return files
+}
+
 type FileType struct {
 	Key             string                `yaml:"-"`
 	PathPatterns    []matcher.FilePattern `yaml:"path"`
@@ -70,6 +86,15 @@ func (ft *FileType) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	ft.RelatedKeys = stringOrSlice(d["related"])
 
 	return nil
+}
+
+func (ft FileType) isExcluded(filePath string) bool {
+	for _, ex := range ft.ExcludePatterns {
+		if ex.Match(filePath) != nil {
+			return true
+		}
+	}
+	return false
 }
 
 func patternOrSlice(in interface{}) []matcher.FilePattern {
